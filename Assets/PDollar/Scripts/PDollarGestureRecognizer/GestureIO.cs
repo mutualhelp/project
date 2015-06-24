5e561473e30ace609ca3c87b3ff3031c5915fe5f
Kimsanggu <sanggoo2821@naver.com>
2015-06-25 03:38:50 +0900

    xml I/O

---
+++ Assets/PDollar/Scripts/PDollarGestureRecognizer/GestureIO.cs
@@ -0,0 +1,199 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml;
+using System.Collections;
+using System;
+
+using UnityEngine;
+
+namespace PDollarGestureRecognizer
+{
+	public class GestureIO
+	{
+		public static Gesture ReadGestureFromXML(string xml) {
+			
+			XmlTextReader xmlReader = null;
+			Gesture gesture = null;
+			
+			try {
+				
+				xmlReader = new XmlTextReader(new StringReader(xml));
+				gesture = ReadGesture(xmlReader);
+				
+			} finally {
+				
+				if (xmlReader != null)
+					xmlReader.Close();
+			}
+			
+			return gesture;
+		}
+		public static Gesture ReadGestureFromFile(string fileName) {
+			
+			XmlTextReader xmlReader = null;
+			Gesture gesture = null;
+			
+			try {
+				
+				xmlReader = new XmlTextReader(File.OpenText(fileName));
+				gesture = ReadGesture(xmlReader);
+				
+			} finally {
+				
+				if (xmlReader != null)
+					xmlReader.Close();
+			}
+			
+			return gesture;
+		}
+		
+		private static Gesture ReadGesture(XmlTextReader xmlReader)
+		{
+			List<Point> points = new List<Point>();
+			int currentStrokeIndex = -1;
+			string gestureName = "";
+			try
+			{
+				while (xmlReader.Read())
+				{
+					if (xmlReader.NodeType != XmlNodeType.Element) continue;
+					switch (xmlReader.Name)
+					{
+					case "Gesture":
+						gestureName = xmlReader["Name"];
+						if (gestureName.Contains("~")) 
+							gestureName = gestureName.Substring(0, gestureName.LastIndexOf('~'));
+						if (gestureName.Contains("_")) 
+							gestureName = gestureName.Replace('_', ' ');
+						break;
+					case "Stroke":
+						currentStrokeIndex++;
+						break;
+					case "Point":
+						points.Add(new Point(
+							float.Parse(xmlReader["X"]),
+							float.Parse(xmlReader["Y"]),
+							currentStrokeIndex
+							));
+						break;
+					}
+				}
+			}
+			finally
+			{
+				if (xmlReader != null)
+					xmlReader.Close();
+			}
+			
+			return new Gesture(points.ToArray(), gestureName);
+		}
+		
+		public static void WriteGesture(PDollarGestureRecognizer.Point[] points, string gestureName, string fileName)
+		{
+			using (StreamWriter sw = new StreamWriter(fileName))
+			{
+				sw.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"yes\"?>");
+				sw.WriteLine("<Gesture Name = \"{0}\">", gestureName);
+				int currentStroke = -1;
+				for (int i = 0; i < points.Length; i++)
+				{
+					if (points[i].StrokeID != currentStroke)
+					{
+						if (i > 0)
+							sw.WriteLine("\t</Stroke>");
+						sw.WriteLine("\t<Stroke>");
+						currentStroke = points[i].StrokeID;
+					}
+					
+					sw.WriteLine("\t\t<Point X = \"{0}\" Y = \"{1}\" T = \"0\" Pressure = \"0\" />",
+					             points[i].X, points[i].Y
+					             );
+				}
+				sw.WriteLine("\t</Stroke>");
+				sw.WriteLine("</Gesture>");
+			}
+		}
+		public static PointList ReadGestureFromXMLForView(string xml) {
+			
+			XmlTextReader xmlReader = null;
+			PointList pointsgesture = null;
+			
+			try {
+				
+				xmlReader = new XmlTextReader(new StringReader(xml));
+				pointsgesture = ReadGestureForView(xmlReader);
+				
+			} finally {
+				
+				if (xmlReader != null)
+					xmlReader.Close();
+			}
+			
+			return pointsgesture;
+		}
+		public static PointList ReadGestureFromFileForView(string fileName) {
+			
+			XmlTextReader xmlReader = null;
+			PointList pointsgesture = null;
+			
+			try {
+				
+				xmlReader = new XmlTextReader(File.OpenText(fileName));
+				pointsgesture = ReadGestureForView(xmlReader);
+				
+			} finally {
+				
+				if (xmlReader != null)
+					xmlReader.Close();
+			}
+			
+			return pointsgesture;
+		}
+		private static PointList ReadGestureForView(XmlTextReader xmlReader)
+		{
+			List<Point> points = null;
+			int currentStrokeIndex = -1;
+			string gestureName = "";
+			try
+			{
+				while (xmlReader.Read())
+				{
+					if (xmlReader.NodeType != XmlNodeType.Element) continue;
+					switch (xmlReader.Name)
+					{
+					case "Gesture":
+						gestureName = xmlReader["Name"];
+						if (gestureName.Contains("~")) 
+							gestureName = gestureName.Substring(0, gestureName.LastIndexOf('~'));
+						if (gestureName.Contains("_")) 
+							gestureName = gestureName.Replace('_', ' ');
+						break;
+					case "Stroke":
+						currentStrokeIndex++;
+						break;
+					case "Point":
+						try{
+							points.Add(new Point(
+								float.Parse(xmlReader["X"]),
+								float.Parse(xmlReader["Y"]),
+								currentStrokeIndex
+								));
+						}catch(System.NullReferenceException e){}
+						break;
+					}
+				}
+			}
+			finally
+			{
+				if (xmlReader != null)
+					xmlReader.Close();
+			}
+			try{
+				return new PointList(points.ToArray(), gestureName);
+			}catch(System.NullReferenceException e){}
+			return null;
+		}
+		
+	}
+}
